4f10b041288dc94a10d35d3dbc5978c5b90eb747
Kunihiko Miyanaga <miyanaga@ideamans.com>
2025-09-27 04:20:12 +0900

    docker.shではamd64のイメージを強制的にビルド

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ LOCAL_IMAGE=banner-generator
 VERSION=$(cat package.json | jq -r .version)
 DOCKERHUB_IMAGE=${DOCKERHUB_IMAGE:-"ideamans/banner-generator"}
 
-docker build -t $LOCAL_IMAGE .
+docker build --platform linux/amd64 -t $LOCAL_IMAGE .
 
 for TAG in $VERSION latest; do
   docker tag $LOCAL_IMAGE $DOCKERHUB_IMAGE:$TAG
